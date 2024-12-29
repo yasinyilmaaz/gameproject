6f2357febe623c01029681208c09aa6fa533904e
yasinyilmaaz <23010903059@subu.edu.tr>
2024-12-29 22:32:18 +0300

    degiskenlerdeki turkce karakterler kaldırıldı

--- main.cpp
+++ main.cpp
@@ -5,8 +5,17 @@ using namespace std;
 
 int main()
 {
-	Uygulama uygulama;
-	uygulama.insaEt(1280, 720);
-	uygulama.baslat();
+	try
+	{
+		Uygulama uygulama;
+		uygulama.insaEt(1280, 720);
+		uygulama.baslat();
+	}
+	catch (const std::exception& err)
+	{
+		cout << "Hata olustu" << endl;
+		cout << "HATA: " << err.what() << endl;
+	}
+	return 0;
 
 }
